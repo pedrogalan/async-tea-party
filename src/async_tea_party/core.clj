05d44619a7b39d1d841456e08d96dd581f35c1cc
Pedro Galan <pedro.galan@piksel.com>
2020-04-09 13:00:15 +0100

    Add tea party example

--- src/async_tea_party/core.clj
+++ src/async_tea_party/core.clj
@@ -1,6 +1,29 @@
-(ns async-tea-party.core)
+(ns async-tea-party.core
+  (:require [clojure.core.async :as async]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def google-tea-service-chan
+  (async/chan 10))
+
+(def yahoo-tea-service-chan
+  (async/chan 10))
+
+(defn random-add []
+  (reduce + (conj [] (repeat 1 (rand-int 100000)))))
+
+(defn request-google-tea-service []
+  (async/go
+    (random-add)
+    (async/>! google-tea-service-chan "tea compliments of google")))
+
+(defn request-yahoo-tea-service []
+  (async/go
+    (random-add)
+    (async/>! yahoo-tea-service-chan "tea compliments of yahoo")))
+
+(defn request-tea []
+  (request-google-tea-service)
+  (request-yahoo-tea-service)
+  (async/go (let [[v] (async/alts!
+                          [google-tea-service-chan
+                           yahoo-tea-service-chan])]
+              (println v))))
